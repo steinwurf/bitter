b6b5fd351a8c014e26ea926f472d37a53f3aa9af
Lars Nielsen <lnc13.lars@gmail.com>
2016-08-03 15:35:32 +0200

    [bitfield_reader_test.cpp]:edit:added new test cases

--- test/src/bitfield_reader_test.cpp
+++ test/src/bitfield_reader_test.cpp
@@ -47,3 +47,138 @@ TEST(test_bit_reader, read_bit)
     value = reader.get<uint8_t, 4>();
     EXPECT_EQ(165U, value);
 }
+
+TEST(test_bit_reader, read_bit2)
+{
+    std::vector<uint8_t> data
+    {
+        0x00, //0000 0000
+        0xFF, //1111 1111
+        0xF0, //1111 0000
+        0x0F, //0000 1111
+        0xA5, //1010 0101
+        0xF0, //1111 0000
+    };
+
+    auto reader =
+         bitter::bitfield_reader<16,16,16>(data.data(), data.size() * 8);
+
+    // Test that groups have correct offset
+    EXPECT_EQ(0U, reader.offset<0>());
+    EXPECT_EQ(16U, reader.offset<1>());
+    EXPECT_EQ(32U, reader.offset<2>());
+
+    // Test that groups have correct size
+    EXPECT_EQ(16U, reader.group_size<0>());
+    EXPECT_EQ(16U, reader.group_size<1>());
+    EXPECT_EQ(16U, reader.group_size<2>());
+
+    auto value = reader.get<uint16_t, 0>();
+    EXPECT_EQ(255U, value);
+
+    value = reader.get<uint16_t, 1>();
+    EXPECT_EQ(61455U, value);
+
+    value = reader.get<uint16_t, 2>();
+    EXPECT_EQ(42480U, value);
+}
+
+TEST(test_bit_reader, read_bit3)
+{
+    std::vector<uint8_t> data
+    {
+        0x00, //0000 0000
+        0xFF, //1111 1111
+        0xF0, //1111 0000
+        0x0F, //0000 1111
+        0xA5, //1010 0101
+        0xF0, //1111 0000
+        0x0F, //0000 1111
+        0xA5, //1010 0101
+    };
+
+    auto reader =
+         bitter::bitfield_reader<32,32>(data.data(), data.size() * 8);
+
+    // Test that groups have correct offset
+    EXPECT_EQ(0U, reader.offset<0>());
+    EXPECT_EQ(32U, reader.offset<1>());
+
+    // Test that groups have correct size
+    EXPECT_EQ(32U, reader.group_size<0>());
+    EXPECT_EQ(32U, reader.group_size<1>());
+
+    auto value = reader.get<uint32_t, 0>();
+    EXPECT_EQ(16773135U, value);
+
+    value = reader.get<uint32_t, 1>();
+    EXPECT_EQ(2783973285U, value);
+}
+
+TEST(test_bit_reader, read_bit4)
+{
+    std::vector<uint8_t> data
+    {
+        0x00, //0000 0000
+        0xFF, //1111 1111
+        0xF0, //1111 0000
+        0x0F, //0000 1111
+        0xA5, //1010 0101
+        0xF0, //1111 0000
+        0x0F, //0000 1111
+        0xA5, //1010 0101
+    };
+
+    auto reader =
+         bitter::bitfield_reader<64>(data.data(), data.size() * 8);
+
+    // Test that groups have correct offset
+    EXPECT_EQ(0U, reader.offset<0>());
+
+    // Test that groups have correct size
+    EXPECT_EQ(64U, reader.group_size<0>());
+    //
+    //
+    auto value = reader.get<uint64_t, 0>();
+    EXPECT_EQ(72040069060366245U, value);
+}
+
+TEST(test_bit_reader, read_bit5)
+{
+    std::vector<uint8_t> data
+    {
+        0xA0, //1010 0000
+    };
+
+    auto reader =
+         bitter::bitfield_reader<1>(data.data(), data.size() * 8);
+
+    // Test that groups have correct offset
+    EXPECT_EQ(0U, reader.offset<0>());
+
+    // Test that groups have correct size
+    EXPECT_EQ(1U, reader.group_size<0>());
+
+     auto value = reader.get<bool, 0>();
+    EXPECT_EQ(true, value);
+}
+
+TEST(test_bit_reader, read_bit6)
+{
+    std::vector<uint8_t> data
+    {
+        0x00, //0000 0000
+    };
+
+    auto reader =
+         bitter::bitfield_reader<1>(data.data(), data.size() * 8);
+
+    // Test that groups have correct offset
+    EXPECT_EQ(0U, reader.offset<0>());
+
+    // Test that groups have correct size
+    EXPECT_EQ(1U, reader.group_size<0>());
+
+     auto value = reader.get<bool, 0>();
+    EXPECT_EQ(false, value);
+}

05fbab2704d607c5823b4e5fe51248971de28084
Lars Nielsen <lnc13.lars@gmail.com>
2016-09-01 14:17:01 +0200

    moving back

--- src/bitter/bitfield_reader.hpp
+++ src/bitter/bitfield_reader.hpp
@@ -7,36 +7,84 @@
 #include <cstdint>
 #include <vector>
 #include <cassert>
-#include <typeinfo>
+#include <type_traits>
 
 #include <endian/big_endian.hpp>
 #include <endian/little_endian.hpp>
+#include <endian/is_big_endian.hpp>
 
 namespace bitter
 {
 template<class EndianType, uint32_t... Groups>
-class bitfield_reader
+class bitfield_writer
 {
 public:
-    bitfield_reader(std::vector<uint8_t> data):
-    m_data(data)
+    bitfield_writer(std::vector<uint8_t> data, uint64_t size):
+    m_data(data),
+    m_size(size)
     {
-        m_bits = data.size() * 8;
+        m_number_of_groups = sizeof...(Groups);
     }
 
-
-    template<typename ReturnType, uint32_t Group>
-    ReturnType read()
+    template<uint32_t Group, typename Type>
+    void write(Type data)
     {
-        auto current_group_size = group_size<Group>();
-        auto current_offset = offset<Group>();
-        return read_bits_from_offset<ReturnType>(current_group_size, current_offset);
+        // Check if Group actually exists
+        //std::cout << "number of groups" << m_number_of_groups << std::endl;
+        assert(Group < m_number_of_groups);
+
+        // Determine offset for group and size for the group
+        auto offset = group_offset<Group>();
+        auto size = group_size<Group>();
+
+        if(std::is_same<Type, bool>::value)
+        {
+            // If the bool value is true, write 0000 0001
+            // If the bool value is false, write 0000 0000
+            if(data)
+            {
+                write_data<uint8_t>(1U, offset, size);
+            }
+            else
+            {
+                write_data<uint8_t>(0U, offset, size);
+            }
+        }
+        else
+        {
+            // Based
+            if(size <= 8)
+            {
+                write_data<uint8_t>(data, offset, size);
+            }
+            else if(size <= 16)
+            {
+                write_data<uint16_t>(data, offset, size);
+            }
+            else if(size <= 32)
+            {
+                write_data<uint32_t>(data, offset, size);
+            }
+            else if(size <= 64)
+            {
+                write_data<uint64_t>(data, offset, size);
+            }
+        }
+
     }
 
-    uint64_t size() const
+    std::vector<uint8_t> data()
     {
-        return m_bits;
+        return m_data;
     }
+
+    template<typename Type>
+    Type data_as_uint()
+    {
+        assert(sizeof(type) == m_size / 8);
+        return EndianType::get<Type>(m_data.data());
+    }
+
 
 private:
     template<uint32_t Group>
@@ -45,16 +93,10 @@ private:
         return group_size_<Group, Groups...>();
     }
 
-    template<uint32_t Group>
-    uint64_t offset()
-    {
-        return offset_<Group, Groups...>();
-    }
-
     template<uint32_t Group, uint32_t NextGroup, uint32_t... InputGroups>
     uint64_t group_size_()
     {
-        if((sizeof...(Groups) - Group) == sizeof...(InputGroups) + 1)
+        if(sizeof...(Groups) - Group == sizeof...(InputGroups) + 1)
         {
             return NextGroup;
         }
@@ -70,68 +112,98 @@ private:
         return 0;
     }
 
-    template<uint32_t Group, uint32_t NextGroup, uint32_t... InputGroups>
-    uint64_t offset_()
+    template<uint32_t Group>
+    uint64_t group_offset()
     {
-        if((sizeof...(Groups) - Group) == sizeof...(InputGroups) + 1)
+        return group_offset_<Group, Groups...>();
+    }
+
+    template <uint32_t Group, uint32_t NextGroup, uint32_t... InputGroups>
+    uint64_t group_offset_()
+    {
+        if(sizeof...(Groups) - Group == sizeof...(InputGroups) + 1)
         {
             return 0;
         }
         else
         {
-            return NextGroup + offset_<Group, InputGroups...>();
+            return NextGroup + group_offset_<Group, InputGroups...>();
         }
     }
 
     template<uint32_t Group>
-    uint64_t offset_()
+    uint64_t group_offset_()
     {
         return 0;
     }
 
-    template<typename ReturnType>
-    ReturnType read_bits_from_offset(uint64_t bits, uint64_t offset)
+    uint64_t create_mask(uint32_t size)
     {
-        std::vector<uint8_t> data_vector;
-        data_vector.resize(bits / 8);
-        auto current_offset = offset / 8;
-        for(int i = current_offset; i < bits / 8; ++i)
+        assert(size <= 64);
+        return ~(0xFFFFFFFFFFFFFFFF >> (64 - size));
+    }
+
+    void write_bit(uint8_t value, uint8_t& dest, uint8_t position)
+    {
+        assert(value == 0 || value == 1);
+        uint8_t mask = 0x001 << (7 - position);
+
+        if(value == 0)
         {
-            data_vector.push_back(m_data[i]);
+            dest &= ~mask;
         }
-        ReturnType value = EndianType::get<ReturnType>(data_vector.data());
-
-    }
-
-
-    uint8_t read_bit_at_offset(uint8_t offset)
-    {
-        assert(offset < m_bits);
-        auto byte = offset / 8;
-        auto position = 7 - (offset % 8);
-        return (m_data[byte] >> position) & 0x1;
-    }
-
-    template<uint32_t Group>
-    bool is_endian_shift_needed()
-    {
-        if(std::is_same<EndianType, endian::little_endian()>())
+        else if(value == 1)
         {
-            auto size = group_size<Group>();
-            if(size == 8 || size == 16 || size == 32 || size == 64)
+            dest |= mask;
+        }
+    }
+
+    template<typename Type>
+    void write_data(Type value_to_write, uint64_t offset, uint64_t size)
+    {
+
+        uint64_t checker_mask = create_mask(size);
+        assert((checker_mask & value_to_write) == 0);
+
+        value_to_write = value_to_write << ((sizeof(value_to_write) * 8) - size);
+
+        uint8_t* data_to_write = (uint8_t*) &value_to_write;
+        uint32_t data_to_write_size = sizeof(value_to_write);
+
+        uint64_t current_offset = offset;
+        for(int i = data_to_write_size - 1; i >= 0; --i)
+        {
+            auto data_to_write_element = data_to_write[i];
+
+            for(int j = 0; j < 8; j++)
             {
-                return false;
-            }
-            else
-            {
-                return true;
+                auto position = 7 - j;
+                auto bit = (data_to_write_element >> position) & 0x1;
+
+                uint64_t byte_offset = current_offset / 8;
+                uint64_t bit_offset = current_offset % 8;
+                auto& value = m_data[byte_offset];
+
+                write_bit(bit, value, bit_offset);
+                ++current_offset;
+                if((current_offset - offset) > size)
+                {
+                    return;
+                }
             }
         }
-        return false;
     }
+
+    bool is_same_endianness()
+    {
+        bool endian_type = std::is_same<EndianType, endian::big_endian>();
+        return is_big_endian == endian_type;
+    }
+
 
 private:
     std::vector<uint8_t> m_data;
-    uint64_t m_bits;
+    uint64_t m_size;
+    uint32_t m_number_of_groups;
 };
 }
--- src/bitter/bitfield_writer.hpp
+++ src/bitter/bitfield_writer.hpp
@@ -7,64 +7,60 @@
 #include <cstdint>
 #include <vector>
 #include <cassert>
-#include <type_traits>
-
-#include <iostream>
+#include <typeinfo>
 
 #include <endian/big_endian.hpp>
 #include <endian/little_endian.hpp>
 
 namespace bitter
 {
-template<class EndianType, uint32_t... Groups>
-class bitfield_writer
+template<class EndianType, typename InputType, uint32_t... Groups>
+class bitfield_reader
 {
 public:
-    bitfield_writer(std::vector<uint8_t> data):
-    m_data(data)
+    bitfield_reader(InputType value)
     {
-        m_number_of_groups = sizeof...(Groups);
+        assert(sizeof(InputType) == total_size_of_groups / 8);
+        m_data.resize(sizeof(InputType));
+        EndianType::template put<InputType>(value, m_data.data());
     }
 
     template<typename Type, uint32_t Group>
-    void write(Type value)
+    Type read()
     {
-        static_assert(std::is_same<Type, uint8_t>() ||
-                      std::is_same<Type, uint16_t>() ||
-                      std::is_same<Type, uint32_t>() ||
-                      std::is_same<Type, uint64_t>(), "Invalid data type");
-
-        assert(sizeof(Type) * 8 == group_size<Group>());
-
-        auto offset = group_offset<Group>();
-        auto size = group_size<Group>();
-
-        auto checker_mask = create_mask(size);
-        assert((checker_mask & value) == 0);
-
-        std::vector<uint8_t> data_vector;
-        data_vector.resize(sizeof(Type));
-
-        EndianType::put(value, data_vector.data());
-        writer_data(data_vector.data(), offset, size);
-    }
-
-    std::vector<uint8_t> data()
-    {
-        return m_data;
+        auto current_group_size = group_size<Group>();
+        auto current_offset = offset<Group>();
+        return read_bits_from_offset<Type>(current_group_size, current_offset);
     }
 
 private:
-    template<uint32_t Group>
-    uint32_t group_size()
+    uint32_t total_size_of_groups()
     {
-        return group_size<Group, Groups...>();
+        return total_size_of_groups_<Groups...>();
+    }
+
+    template<uint32_t Group, uint32_t... InputGroups>
+    uint32_t total_size_of_groups_()
+    {
+        return Group + total_size_of_groups_<InputGroups...>();
+    }
+
+    template<uint32_t Group>
+    uint32_t total_size_of_groups_()
+    {
+        return 0;
+    }
+
+    template<uint32_t Group>
+    uint64_t group_size()
+    {
+        return group_size_<Group, Groups...>();
     }
 
     template<uint32_t Group, uint32_t NextGroup, uint32_t... InputGroups>
-    uint32_t group_size_()
+    uint64_t group_size_()
     {
-        if (sizeof...(Groups) - Group == sizeof...(InputGroups) + 1)
+        if((sizeof...(Groups) - Group) == sizeof...(InputGroups) + 1)
         {
             return NextGroup;
         }
@@ -75,99 +71,58 @@ private:
     }
 
     template<uint32_t Group>
-    uint32_t group_size_()
+    uint64_t group_size_()
     {
         return 0;
     }
 
     template<uint32_t Group>
-    uint32_t group_offset()
+    uint64_t offset()
     {
-        return group_offset_<Group, Groups...>();
+        return offset_<Group, Groups...>();
     }
 
     template<uint32_t Group, uint32_t NextGroup, uint32_t... InputGroups>
-    uint32_t group_offset_()
+    uint64_t offset_()
     {
-        if (sizeof...(Groups) - Group == sizeof...(InputGroups) + 1)
+        if((sizeof...(Groups) - Group) == sizeof...(InputGroups) + 1)
         {
             return 0;
         }
         else
         {
-            return NextGroup + group_offset_<Group, InputGroups...>();
+            return NextGroup + offset_<Group, InputGroups...>();
         }
-
     }
 
     template<uint32_t Group>
-    uint32_t group_offset_()
+    uint64_t offset_()
     {
         return 0;
     }
 
-    bool check_valid_group_size(uint32_t Group)
+    template<typename ReturnType>
+    ReturnType read_bits_from_offset(uint64_t bits, uint64_t offset)
     {
-        if (group_size<Group>() == 8 || group_size<Group>() == 16 ||
-            group_size<Group>() == 32 || group_size<Group>() == 64)
+        assert(bits <= 64);
+        ReturnType result = 0;
+        for(uint64_t i = 0; i < bits; ++i)
         {
-                return true;
+            ReturnType bit = read_bit_at_offset(offset + i);
+            result |=  bit << (bits - i - 1);
         }
-        else
-        {
-            return false;
-        }
+        return result;
     }
 
-    uint64_t create_mask(uint32_t size)
+    uint8_t read_bit_at_offset(uint8_t offset)
     {
-        assert(size <= 64);
-        return ~(0xFFFFFFFFFFFFFFFF >> (64 - size));
+        assert(offset < sizeof(InputType) * 8);
+        auto byte = offset / 8;
+        auto position = 7 - (offset % 8);
+        return (m_data[byte] >> position) & 0x1;
     }
-
-    void write_bit(uint8_t value, uint8_t& dest, uint8_t position)
-    {
-        assert(value == 0 || value == 1);
-        uint8_t mask = 0x001 << (7 - position);
-
-        if(value == 0)
-        {
-            dest &= ~mask;
-        }
-        else if(value == 1)
-        {
-            dest |= mask;
-        }
-    }
-
-    void write_data(uint8_t* data, uint32_t offset, uint32_t size)
-    {
-        uint32_t current_offset = offset;
-        for (int i = size - 1; i >= 0; --i)
-        {
-            auto element_to_write = data[i];
-            for (int j = 0; j < 8; ++i)
-            {
-                auto position = 7 - j;
-                auto bit = (element_to_write >> position) & 0x1;
-
-                auto byte_offset = current_offset / 8;
-                auto bit_offset = current_offset % 8;
-                auto& value = m_data[byte_offset];
-                write_bit(bit, value, bit_offset);
-                ++current_offset;
-                if ((current_offset - offset) > size)
-                {
-                    return;
-                }
-            }
-        }
-    }
-
 
 private:
     std::vector<uint8_t> m_data;
-    uint64_t m_size;
-    uint32_t m_number_of_groups;
 };
 }

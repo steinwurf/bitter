df0ea5093962b9bc1d0fa23b0c65e0175b2b506d
Lars Nielsen <lnc13.lars@gmail.com>
2016-09-04 22:19:51 +0200

    testing new part of hack

--- test/src/bitfield_reader_test.cpp
+++ test/src/bitfield_reader_test.cpp
@@ -37,10 +37,15 @@ TEST(test_bit_reader, read_bit)
         EXPECT_EQ(reader.data_ptr()[1], 255U);
         EXPECT_EQ(reader.data_ptr()[2], 240U);
         EXPECT_EQ(reader.data_ptr()[3], 15U);
-        EXPECT_EQ(reader.read<uint8_t, 0>, 0U);
-        EXPECT_EQ(reader.read<uint8_t, 1>, 255U);
-        EXPECT_EQ(reader.read<uint8_t, 2>, 240U);
-        EXPECT_EQ(reader.read<uint8_t, 3>, 15U);
+
+        auto value = reader.read<uint8_t, 0>();
+        EXPECT_EQ(value, 0U);
+        value = reader.read<uint8_t, 1>();
+        EXPECT_EQ(value, 255U);
+        value = reader.read<uint8_t, 2>();
+        EXPECT_EQ(value, 240U);
+        value = reader.read<uint8_t, 3>();
+        EXPECT_EQ(value, 15U);
     }
     else
     {
@@ -48,10 +53,15 @@ TEST(test_bit_reader, read_bit)
         EXPECT_EQ(reader.data_ptr()[1], 255U);
         EXPECT_EQ(reader.data_ptr()[2], 240U);
         EXPECT_EQ(reader.data_ptr()[3], 15U);
-        EXPECT_EQ(reader.read<uint8_t, 0>, 0U);
-        EXPECT_EQ(reader.read<uint8_t, 1>, 255U);
-        EXPECT_EQ(reader.read<uint8_t, 2>, 240U);
-        EXPECT_EQ(reader.read<uint8_t, 3>, 15U);
+        auto value = reader.read<uint8_t, 0>();
+        EXPECT_EQ(value, 0U);
+        value = reader.read<uint8_t, 1>();
+        EXPECT_EQ(value, 255U);
+        value = reader.read<uint8_t, 2>();
+        EXPECT_EQ(value, 240U);
+        value = reader.read<uint8_t, 3>();
+        EXPECT_EQ(value, 15U);
+
     }
 
 

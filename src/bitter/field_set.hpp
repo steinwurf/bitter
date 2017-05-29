2d7fbbd14600a870da825718d019e3d508a58c26
Jeppe Krigslund <krigslund@steinwurf.com>
2017-05-29 16:35:10 +0200

    added compile-time compute of bitfield limit

--- src/bitter/field_set.hpp
+++ src/bitter/field_set.hpp
@@ -7,6 +7,7 @@
 #include <cassert>
 #include <cmath>
 #include <cstdint>
+#include <type_traits>
 
 #include "field_mask.hpp"
 #include "field_offset.hpp"
@@ -14,18 +15,31 @@
 
 namespace bitter
 {
+namespace detail
+{
+inline constexpr std::size_t pow2(unsigned const exponent)
+{
+    // (parentheses not required in next line)
+    return (exponent == 0) ? 1 : (2 * pow2(exponent - 1));
+}
+}
+
 /// @brief set the value in a bitfield based on the index
 /// @param bitfield is the data of the writer
 /// @param value is the data we want to write to the bitfield
 template<class DataType, uint32_t Index, uint32_t... Sizes>
 DataType field_set(DataType bitfield, DataType value)
 {
+    using bitfield_limit =
+        std::integral_constant<std::size_t,
+        detail::pow2(
+            field_size_in_bits<Index, Sizes...>())>;
+    // Verify that the bitfield can be represented with the available bits:
+    assert(value < bitfield_limit::value &&
+           "value exceeds limit representable by available bits");
+
     uint32_t offset = field_offset<Index, Sizes...>();
     DataType mask = field_mask<DataType, Index, Sizes...>();
-
-    // Verify that the bitfield can be represented with the available bits:
-    assert(value < std::pow(2, field_size_in_bits<Index, Sizes...>()) &&
-           "value exceeds limit representable by available bits");
 
     // Shift the value up to where it should go
     // and do the same with the mask

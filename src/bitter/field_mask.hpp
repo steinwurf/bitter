86bd5243c924fe24b7608509d2dd521ea2fe6a78
Jeppe Krigslund <krigslund@steinwurf.com>
2017-05-30 09:49:56 +0200

    cleanup

--- src/bitter/field_mask.hpp
+++ src/bitter/field_mask.hpp
@@ -15,7 +15,7 @@ namespace bitter
 /// @brief Function for creating a maske for variable
 ///        with the sizeof DataTypes
 template<class DataType, uint32_t Index, uint32_t... Sizes>
-constexpr DataType field_mask()
+DataType field_mask()
 {
     uint32_t field_size = field_size_in_bits<Index, Sizes...>();
     uint32_t data_type_size = size_in_bits<DataType>();

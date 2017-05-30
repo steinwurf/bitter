bb9b98d5a4c6d9363b559ccd1ba9786a66c29715
Morten V. Pedersen <morten@mortenvp.com>
2017-05-30 09:09:40 +0200

    Creating a max value function

--- src/bitter/field_set.hpp
+++ src/bitter/field_set.hpp
@@ -17,15 +17,6 @@
 
 namespace bitter
 {
-namespace detail
-{
-template<typename T>
-inline constexpr T pow2(const T exponent)
-{
-    // (parentheses not required in next line)
-    return (exponent == 0) ? 1 : (2 * pow2(exponent - 1));
-}
-}
 
 /// @brief set the value in a bitfield based on the index
 /// @param bitfield is the data of the writer
@@ -33,12 +24,8 @@ inline constexpr T pow2(const T exponent)
 template<class DataType, uint32_t Index, uint32_t... Sizes>
 DataType field_set(DataType bitfield, DataType value)
 {
-    using bitfield_limit =
-        std::integral_constant<uint64_t,
-        detail::pow2((uint64_t)field_size_in_bits<Index, Sizes...>())>;
-
     // Verify that the bitfield can be represented with the available bits:
-    assert(value < bitfield_limit::value &&
+    assert(value < field_max_value<DataType, Index, Sizes...>() &&
            "value exceeds limit representable by available bits");
 
     uint32_t offset = field_offset<Index, Sizes...>();
